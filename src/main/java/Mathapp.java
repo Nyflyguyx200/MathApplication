e0685588ea85caac60d1716c8ac3dcf77eefdd9a
Seth Castro <sethcastro50@yahoo.com>
2024-04-11 14:34:46 -0400

    work complete

---
+++ src/main/java/Mathapp.java
@@ -0,0 +1,27 @@
+public class Mathapp {
+    public static void main(String[] args) {
+        //Question 1
+        int garySalary = 40000;
+        int bobSalary = 200;
+
+        int highestSalary = Math.max(garySalary, bobSalary);
+
+        System.out.println("The Highest Salary is " + highestSalary);
+
+        //Question 2
+        double carPrice = 40000;
+        double truckPrice = 200;
+
+        double smallestSalary = Math.min(carPrice, truckPrice);
+
+        System.out.println("The Smallest Salary is " + smallestSalary);
+
+        //Question 3
+        double radius = 7.25;
+        double areaOfCircle = Math.PI * Math.pow(radius, 2);
+
+        System.out.println("Area of a circle with " + radius + " is cd p" + areaOfCircle);
+
+        //Question 4
+    }
+}
